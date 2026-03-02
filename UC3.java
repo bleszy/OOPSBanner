8bb946943e211adaf9d72e3e96375ba58558f088
bleszy <bb1487@srmist.edu.in>
2026-03-03 00:09:59 +0530

    UC2

--- UC3.java
+++ UC3.java
@@ -2,12 +2,12 @@ public class UC3 {
   
   void main()
 {
-  System.out.println(String.join(" ", "   ***   ","   ***** "));
-  System.out.println(String.join(" "," **   ** "," **      "));
-  System.out.println(String.join(" ","**     **","**       "));
-  System.out.println(String.join(" ","**     **","  *****  "));
-  System.out.println(String.join(" ","**     **","      ** "));
-  System.out.println(String.join(" "," **   ** "," **   ** "));
-  System.out.println(String.join(" ","   ***   ","  *****  "));
+  System.out.println(String.join(" ", "   ***   ","   ***   ","******   ","   ***** "));
+  System.out.println(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
+  System.out.println(String.join(" ","**     **","**     **","**     **","**       "));
+  System.out.println(String.join(" ","**     **","**     **","**    ** ","  *****  "));
+  System.out.println(String.join(" ","**     **","**     **","******   ","      ** "));
+  System.out.println(String.join(" "," **   ** "," **   ** ","**       "," **   ** "));
+  System.out.println(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
 }
 }
